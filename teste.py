23dd9f187ab6f73450e44e5f03d5e358a1887269
cleyton <cleyton.alves@academico.ifpb.edu.br>
2026-05-13 11:46:34 -0300

    Delete teste.py

--- teste.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-st.image("WhatsApp Image 2026-04-15 at 11.17.27 AM.png")
-st.write("Site Cleyton")
-st.link_button("Acessar", "https://sites.google.com/academico.ifpb.edu.br/cleyton-alves-da-silva/in%C3%ADcio")
